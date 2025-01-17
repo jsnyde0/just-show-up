0318a4234f31f0d80d05eff3b6f0665959e21acd
jsnyde0 <jonatansnyders@gmail.com>
2025-01-17 12:17:04 +0100

    added pytest-django

--- events/tasks.py
+++ events/tasks.py
@@ -1,7 +1,5 @@
 from celery import shared_task
 
-from .crewai.crew import test_openai
-
 
 @shared_task
 def example_event_task():
@@ -10,4 +8,5 @@ def example_event_task():
 
 @shared_task
 def test_openai_task():
-    return test_openai()
+    # return test_openai()
+    print("We should be running a crew but not implemented yet...")
